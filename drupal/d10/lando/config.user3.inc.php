8d898ec1fe6db815413ac47c88872f9f1a56c033
Richard Allen <rallen@bctpartners.com>
2022-02-07 01:31:46 -0500

    ✨ Add current Boilerplate for Drupal 10.
    - 💩 The webroot for drupal is not my favorite, but everything else is good.
    - 🔨 Xdebug for Drush CLI via Lando Tooling (lando dd) is amazing.
    - Includes lando/private.env (.gitignored)
    - Updates to proxy configuration (for Solr)
    - Latest Solr 8 container.
    - Using official PMA container (5.1.1-apache).
    - Additional compression utilities for Maria DB.
    - Base tooling is up-to-date.
    - Composer merge configuration for webform is nice.

---
+++ drupal/d10/lando/config.user3.inc.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Custom configuration file for PMA container.
+ * References:
+ * - https://docs.phpmyadmin.net/en/latest/config.html
+ * - http://localhost:60260/doc/html/config.html#cfg_Servers_ssl_ca
+ */
+
+// Environment variables passed from Lando private.env.
+// $env = &$_SERVER;
+
+declare(strict_types=1);
+$cfg['blowfish_secret'] = 'nm8p94D3ZtcAdoEtYz32sEdka7fsUyQ3';
+
+$cfg['MaxNavigationItems'] = 1000;
+$cfg['NavigationTreeTableSeparator'] = '_';
+$cfg['PmaAbsoluteUri'] = 'https://pma.bd10.lndo.site/';
+
+/**
+ * Servers configuration
+ */
+$i = 0;
+
+/**
+ * First server
+ */
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+
+$cfg['Servers'][$i]['verbose'] = 'lando database';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Servers'][$i]['host'] = 'database';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['nopassword'] = true;
+
+
+# https://docs.phpmyadmin.net/en/latest/config.html#server-connection-settings
+$i++;
+$cfg['Servers'][$i]['verbose'] = 'bd10 cloud';
+$cfg['Servers'][$i]['compress'] = true;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Servers'][$i]['host'] = $_ENV['BD_PMA_HOST'];
+$cfg['Servers'][$i]['user'] = $_ENV['BD_PMA_USER'];
+$cfg['Servers'][$i]['port'] = $_ENV['BD_PMA_PORT'];
+$cfg['Servers'][$i]['password'] = $_ENV['BD_PMA_PASS'];
+$cfg['Servers'][$i]['ssl_ca'] = $_ENV['BD_PMA_SSLCA'];
+$cfg['Servers'][$i]['ssl'] = true;
+$cfg['Servers'][$i]['ssl_verify'] = false;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+
+
+?>
---
+++ drupal/d10/sites/bd10.lndo.site/settings.php
@@ -0,0 +1,39 @@
+<?php
+
+// @codingStandardsIgnoreFile
+
+$databases = [];
+$databases['default']['default'] = [
+   'database' => 'drupal9',
+   'username' => 'drupal9',
+   'password' => 'drupal9',
+   'host' => 'database',
+   'driver' => 'mysql',
+];
+
+$settings['config_sync_directory'] = './config/sync';
+
+$settings['hash_salt'] = 'salty-gnome';
+
+$settings['file_public_path'] = 'sites/bd10.lndo.site/files';
+
+// The default entity_update_batch_size is 50.
+// core/modules/system/system.post_update.php, system_post_update_sort_all_config().
+// Settings::get('entity_update_batch_size', 50);
+//
+// Increase batch size a tad to speed up debugging.
+$settings['entity_update_batch_size'] = 325;
+
+
+// Clear trusted host warning for Lando.
+// https://www.drupal.org/docs/installing-drupal/trusted-host-settings
+$settings['trusted_host_patterns'] = [
+  '^localhost$',
+  '^bd10\.lndo\.site$',
+];
+
+// Drupal file upload by anonymous or untrusted users into public file systems -- PSA-2016-003
+// https://www.drupal.org/psa-2016-003
+// https://www.drupal.org/docs/8/core/modules/file/overview
+$settings['file_private_path'] = 'sites/bd10.lndo.site/private';
+// $settings['file_private_path'] = $app_root . '/../private';
