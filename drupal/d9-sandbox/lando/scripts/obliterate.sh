bbfc85895f96d453467060d2b14c907363ce6248
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-11-14 06:34:26 -0500

    Remove d9-sandbox

--- drupal/d9-sandbox/lando/scripts/obliterate.sh
+++
@@ -1,9 +0,0 @@
-lando destroy -y
-
-docker container stop $(docker container ls -q --filter name="landoproxy*")
-docker container rm $(docker container ls -q -a --filter name="landoproxy*")
-
-docker system prune
-
-# Remove any traefik proxy caching, just in case it gets confused with docker networks.
-rm -rf /home/wsl/.lando/proxy
--- drupal/d9-sandbox/lando/scripts/schema-updates.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-php ${DRUPAL_VENDOR}/bin/drush --root=${DRUPAL_ROOT} cr
-php ${DRUPAL_VENDOR}/bin/drush --root=${DRUPAL_ROOT} cron -y
-php ${DRUPAL_VENDOR}/bin/drush --root=${DRUPAL_ROOT} updb -y
-php ${DRUPAL_VENDOR}/bin/drush --root=${DRUPAL_ROOT} config:import -y
